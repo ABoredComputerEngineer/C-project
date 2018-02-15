9eb2922c420c43c6872fc006ddbcd2fb97b0d09c
Asim Maharjan <maharjan291@gmail.com>
2018-02-15 07:06:30 +0545

    Finished two player, Still need more changes

--- io.c
+++ io.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define BOARD 9
@@ -25,24 +26,32 @@ void delay(double dly){
 
 
 typedef struct game {
-	char mat[BOARD_ROW][BOARD_COL];
-	char row[BOARD]; 
+	char mat[BOARD_ROW][BOARD_COL];// The game board stored in matrix form
+	char row[BOARD]; //The game board stored in a single array
 } game; 
 
+/* Declaration of the Global Variables */
 game gameBoard;
+int turn = 1;
+
 
 typedef struct move {
 	int x;
 	int y;
 } move;
 
+/* Declarations and definitions of functions */
+
+
+/* ctoi converts the character to an integer if it is between 1  and 9*/
 int ctoi( char c ){
 	if ( c > '0' && c<='9' ) 
-		return c - '0';
+		return c - '0' - 1 ;
 	else
 		return -1;
 }
 
+/* Initializes the board to the initial state by setting the entire board to empty*/
 void initializeBoard(void){
 	int i,j;
 	for ( i = 0; i<BOARD_ROW ; i++ )
@@ -51,88 +60,113 @@ void initializeBoard(void){
 	for ( i = 0; i<BOARD; i++ )
 		gameBoard.row[i] = ' ';
 }
+
+
+/* prints the board according to the row type of the game board*/
 void printBoard( ){
 	printf("\t\t         |        |         \n");
-	printf("\t\t    %c    |    %c   |    %c     \n",gameBoard.mat[2][0],gameBoard.mat[2][1],gameBoard.mat[2][2]);
+	printf("\t\t    %c    |    %c   |    %c     \n",gameBoard.row[6],gameBoard.row[7],gameBoard.row[8]);
 	printf("\t\t         |        |         \n");
 	printf("\t\t---------|--------|---------\n");
 	//printf("\t\t         |        |         \n");
 	printf("\t\t         |        |         \n");
-	printf("\t\t    %c    |    %c   |    %c     \n",gameBoard.mat[1][0],gameBoard.mat[1][1],gameBoard.mat[1][2]);
+	printf("\t\t    %c    |    %c   |    %c     \n",gameBoard.row[3],gameBoard.row[4],gameBoard.row[5]);
 	printf("\t\t         |        |         \n");
 	printf("\t\t---------|--------|---------\n"); 	
 	printf("\t\t         |        |         \n");
-	printf("\t\t    %c    |    %c   |    %c     \n",gameBoard.mat[0][0],gameBoard.mat[0][1],gameBoard.mat[0][2]);
+	printf("\t\t    %c    |    %c   |    %c     \n",gameBoard.row[0],gameBoard.row[1],gameBoard.row[2]);
 	printf("\t\t         |        |         \n"); 
 	// printf("\t\t         |        |         \n");
 }
 
+
+/* Checks if the game has ended or not */
+
 int hasEnded( void ){
-	int i,j;
-	// CHECK FOR ROWS
-	for ( i = 0; i<BOARD_ROW; i++ )
-		for ( j = 0; j<BOARD_COL-1; j++ ){
-			if ( gameBoard.mat[i][j] != gameBoard.mat[i][j+1] )
-				return TRUE;
-		}
-	// CHECK FOR COLUMS
-	for  ( j = 0; j<BOARD_COL ; j++ )
-		for( i = 0; i<BOARD_ROW-1; i++ )
-			if ( gameBoard.mat[i+1][j] != gameBoard.mat[i][j] )
-				return TRUE;
-	// CHECK FOR DIAGONALS
-	for ( i = 0; i<BOARD_ROW; i++ )
-		if ( gameBoard.mat[i][i] != gameBoard.mat[i+1][i+1] )
+	int i;
+	/*Checking if the game has ended in any of the row*/
+	for ( i = 0; i<7 ; i+= 3 ){
+		/*If one of cell of the row is empty , the game has not ended in that row*/
+		if (  gameBoard.row[i+1] == ' ')
+			break;
+		/* If all the values in the row are equal , The game has ended */
+		if ( gameBoard.row[i] == gameBoard.row[i+1] && gameBoard.row[i+1] == gameBoard.row[i+2] )
 			return TRUE;
-	if ( (gameBoard.mat[2][0] != gameBoard.mat[1][1] ) || ( gameBoard.mat[0][2] != gameBoard.mat[1][1] ) )
-		return TRUE;
+	}
+	/*Checking if the game has ended in any of the column*/
+	for ( i = 0; i<3 ; i++ ){
+		if ( gameBoard.row[i] == ' ' && gameBoard.row[i+3] == ' ' && gameBoard.row[i+6] == ' ')
+			break;
+		if ( gameBoard.row[i] == gameBoard.row[i+3] && gameBoard.row[i+3] == gameBoard.row[i+6] )
+			return TRUE;
+	}
+	/*Checking if the game has ended in any of the diagonals*/
+	if ( gameBoard.row[4] != ' '){
 
+		if ( gameBoard.row[0] == gameBoard.row[4] && gameBoard.row[4] == gameBoard.row[8] )
+			return TRUE;
+		if ( gameBoard.row[2] == gameBoard.row[4] && gameBoard.row[4] == gameBoard.row[6] )
+			return TRUE;
+	}
+	/*If it has not ended */
 	return FALSE;
+}
+
+void performMove( int x , int player ){
+	/* Changes the value in the game board accoriding to the player, player 1 is 0 
+	and player 2 is + */
+	gameBoard.row[x] = ( player == PLAYER1 )?'0':'+';;
 
 }
 
-void performMove( move *a , int player ){
-	char sym;
-	sym = ( player )?'0':'+';
-	gameBoard.mat[a->y][a->x] = sym;
 
-}
-
+/* Checks if the move the user is trying to do is legal or not */
 int isLegal( int x  ){
-	if ( gameBoard.row[x-1] != ' ' )
+	/* If the character is not valid or the cell the user is trying to fill is not Empty*/
+	if ( x == -1 || gameBoard.row[x] != ' ')
 		return FALSE;
 	else 
 		return TRUE; 
 }
 
+/* Gets the move the user wants to perform in the form of a string and returns the value of the cell
+	in the board */
 
 int getMove( void ){
-	int x;
+	char in[100];
 	while ( TRUE ){
-		x = ctoi(getchar());
-		if (  x != -1  && isLegal(x) )
-			return x;
-			// printf("\nInvalid move\n");
+		scanf("%s",in);
+		if ( strlen(in) == 1 ){
+			if ( isLegal(ctoi(in[0])) )
+				return ctoi(in[0]);
+		//	else 
+		//		printf("\nInvalid Move\n");
+		}// else {
+		//	printf("\nInvalid Move\n");
+		//}
+	printf("\nInvalid Move\n");
 	}
+	
 }
 
 
 int main( ){
 	// delay( 5 );
 	int player = PLAYER1, cell;
-	move newMove;
+	// move newMove;
 	initializeBoard();
 	printBoard();
 
 	while ( !hasEnded() ){
 		cell = getMove();
-		newMove.x = (cell - 1 ) % 3;
-		newMove.y = (cell - 1 )/3;
-		performMove(&newMove, player);
+		// newMove.x = (cell - 1 ) % 3;
+		// newMove.y = (cell - 1 )/3;
+		performMove(cell, player);	
 		printBoard();
+		turn++;
 		player = !player;
 	}
 
-	printf("You won");
+	printf("You won\n");
 	return 0;
 }
